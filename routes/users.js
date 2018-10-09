b68f3cd56d7b41f029115ef86d48b8e5b08a144c
JungWoongPark <myuoong@gmail.com>
2018-10-09 19:34:52 +0900

    user token test

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+function makeResMeg(data){
+    return res = {
+        resCode : '0000'
+        ,resMsg : '정상처리'
+        ,result : data
+    };
+}
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.put('/info/:id', function(req, res, next) {
+    console.log('body => ',req.body);
+
+    res.json(makeResMeg(''));
 });
 
 module.exports = router;
